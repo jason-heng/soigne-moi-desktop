101b49638954c9c1461d38eaa71ee858f881b18b
Jason Heng <jasonheng0304@gmail.com>
2024-08-16 19:45:47 +0100

    feat: convert start and end dates into datetime objects

--- utils/objects.py
+++ utils/objects.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 
 class Patient:
-    def __init__(self, patient_info : dict):
+    def __init__(self, patient_info : dict) -> None:
         self.id = patient_info["id"]
         self.first_name = patient_info["firstName"]
         self.last_name = patient_info["lastName"]
@@ -9,17 +10,17 @@ class Patient:
 
 
 class Doctor:
-    def __init__(self, doctor_info):
+    def __init__(self, doctor_info) -> None:
         self.first_name = doctor_info["firstName"]
         self.last_name = doctor_info["lastName"]
 
 
 
 class Stay:
-    def __init__(self, stay_info : dict):
+    def __init__(self, stay_info : dict) -> None:
         self.id : int = stay_info["id"]
         self.patient = Patient(stay_info["patient"])
         self.doctor = Doctor(stay_info["doctor"])
         self.reason : str = stay_info["reason"]
-        self.start = stay_info["reason"]
-        self.end = stay_info["end"]
+        self.start = datetime.strptime(stay_info["start"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        self.end = datetime.strptime(stay_info["end"], "%Y-%m-%dT%H:%M:%S.%fZ")
