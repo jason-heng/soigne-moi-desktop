c4485a54df41992332725ef4a2738bdcb7c8db02
Jason Heng <jasonheng0304@gmail.com>
2024-08-16 17:19:54 +0100

    feat: add get functions to get info from the website api

--- utils/logic.py
+++ utils/logic.py
@@ -1,19 +1,50 @@
+import json
 from utils.objects import Patient, Stay
-from random import randint
+import requests
 
-def get_all_stays() -> list[Stay] | None:
-    return []
+from config import API_URL
 
-def get_all_patients() -> list[Patient] | None:
-    patients_list = [Patient(randint(0, 100)) for i in range(20)]
+def get_token() -> str | None:
+    with open("session.json", "r") as file:
+        session = json.load(file)
+    return session if session else None
+
+
+def get_all_stays(token : str) -> list[Stay] | None:
+    headers = {"Authorization": f"Bearer {token}"}
+    req = requests.get(
+        url=f"{API_URL}/secretary/stays",
+        headers=headers,
+    )
+    stays_list = []
+
+    for stay_info in req.json()["stays"]:
+        stay = Stay(stay_info)
+        stays_list.append(stay)
+
+    return stays_list
+
+
+def get_all_patients(token : str) -> list[Patient] | None:
+    stays_list = get_all_stays(token)
+    patients_list = []
+    for stay in stays_list:
+        if stay.patient not in patients_list:
+            patients_list.append(stay.patient)
+
     return patients_list
 
-def get_stay(stay_id) -> Stay | None:
-    return []
 
-def get_patient_stays(patient_id) -> list[Stay]:
-    return []
+def get_patient_stays(patient_id, token) -> list[Stay]:
+    stays_list = get_all_stays(token)
+    patient_stays = []
+    for stay in stays_list:
+        if stay.patient.id == patient_id:
+            patient_stays.append(stay)
+    
+    return patient_stays
 
-def get_patient(patient_id) -> Patient | None:
+
+def get_patient(patient_id, token) -> Patient | None:
     return []
 
