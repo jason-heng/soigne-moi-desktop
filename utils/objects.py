122a78cea3ac8b3ebe2b927648457ca250dbfa38
Jason Heng <jasonheng0304@gmail.com>
2024-08-17 23:49:21 +0100

    feat: add Secretary, Opinion, Drug, and Prescription classes

--- utils/objects.py
+++ utils/objects.py
@@ -1,14 +1,17 @@
-from datetime import datetime
 from utils.timeHandling import to_local_datetime
 
+from datetime import datetime
 
 class Patient:
-    def __init__(self, patient_info : dict) -> None:
-        self.id = patient_info["id"]
-        self.first_name = patient_info["firstName"]
-        self.last_name = patient_info["lastName"]
-        self.opinions = patient_info["opinions"]
-        
+    def __init__(self, patient_info : dict, full_patient: bool) -> None:
+        self.id: int = patient_info["id"]
+        self.first_name: str = patient_info["firstName"]
+        self.last_name: str = patient_info["lastName"]
+
+        if full_patient:
+            self.opinions: list[Opinion] = [Opinion(opinion_info) for opinion_info in patient_info["opinions"]]
+            self.stays: list[Stay] = [Stay(stay_info, patient=self) for stay_info in patient_info["stays"]]
+
 
 
 class Doctor:
@@ -19,17 +22,40 @@ class Doctor:
 
 
 class Stay:
-    def __init__(self, stay_info : dict) -> None:
+    def __init__(self, stay_info : dict, patient : Patient | None = None) -> None:
         self.id : int = stay_info["id"]
-        self.patient = Patient(stay_info["patient"])
+        self.patient = patient if patient else Patient(stay_info["patient"], False)
         self.doctor = Doctor(stay_info["doctor"])
         self.reason : str = stay_info["reason"]
         self.start = to_local_datetime(stay_info["start"])
         self.end = to_local_datetime(stay_info["end"])
+        self.prescription = Prescription(stay_info["prescription"])
 
 
 class Secretary:
     def __init__(self, secretary_info : dict) -> None:
         self.id : int = secretary_info["id"]
-        self.first_name : int = secretary_info["firstName"]
-        self.last_name : int = secretary_info["lastName"]
+        self.first_name = secretary_info["firstName"]
+        self.last_name = secretary_info["lastName"]
+
+
+class Opinion:
+    def __init__(self, opinion_info : dict) -> None:
+        self.doctor = Doctor(opinion_info["doctor"])
+        self.title : str = opinion_info["title"]
+        self.description : str = opinion_info["description"]
+
+
+
+class Drug:
+    def __init__(self, drug_info : dict) -> None:
+        self.name = drug_info["name"]
+        self.dosage = drug_info["dosage"]
+
+
+
+class Prescription:
+    def __init__(self, prescription_info : dict) -> None:
+        self.start = to_local_datetime(prescription_info["start"])
+        self.end: datetime = to_local_datetime(prescription_info["end"])
+        self.drugs: list[Drug] = prescription_info["end"]
